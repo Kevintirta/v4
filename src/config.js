46717135267d68d55aa7c6f6b9cc436149399c49
kevin <7318377kevintirta@gmail.com>
2020-08-10 20:28:45 +0800

    change config

--- src/config.js
+++ src/config.js
@@ -1,38 +1,34 @@
 module.exports = {
-  siteTitle: 'Brittany Chiang | Software Engineer',
+  siteTitle: 'Kevin Gondokusumo | Software Engineer',
   siteDescription:
-    'Brittany Chiang is a software engineer based in Boston, MA who specializes in building (and occasionally designing) exceptional websites, applications, and everything in between.',
+    'Kevin is a software engineer who is based in Singapore, he specialized in web app and mobile app development.',
   siteKeywords:
-    'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
+    'Kevin Gondokusumo, Kevin, Gondokusumo, software engineer, front-end engineer, web developer, javascript',
   siteUrl: 'http://Kevintirta.github.io/v4',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
-  name: 'Brittany Chiang',
-  location: 'Boston, MA',
-  email: 'brittany.chiang@gmail.com',
-  github: 'https://github.com/bchiang7',
-  twitterHandle: '@bchiang7',
+  name: 'Kevin Gondokusumo',
+  location: 'Singapore',
+  email: '7318377kevintirta@gmail.com',
+  github: 'https://github.com/Kevintirta/',
+  twitterHandle: '@KevinTirtaGond1',
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/Kevintirta/',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
-    },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://www.linkedin.com/in/kevintirtag/',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/kevintirta98',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/KevinTirtaGond1',
     },
   ],
 
